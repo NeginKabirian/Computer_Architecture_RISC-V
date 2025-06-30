6eb49d32c20f1355eb948a5793b89b02a8b453b7
unknown <yaldajafari383@gmail.com>
2025-06-30 13:06:12 +0430

    try to solve Li assembling problem

--- assembler/assembletest.s
+++ assembler/assembletest.s
@@ -1,7 +1,6 @@
-li x1, 0xAAAA5555
-li x2, 0x12345678
-xor x3, x1, x2
-or  x4, x1, x2
-and x5, x1, x2
-sll x6, x5, x0        
-sra x7, x1, x0        
+li x6, 0x0F0F0F0F   # x6 = 0x0F0F0F0F
+li x7, 0x00FF00FF   # x7 = 0x00FF00FF
+xor x8, x6, x7      # x8 = xor of x6 and x7
+or  x9, x6, x7      # x9 = or
+and x10, x6, x7     # x10 = and
+not x11, x10        # x11 = ~x10 (xori with -1)
