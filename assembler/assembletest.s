d5be19462d60131d264114f9aed7de4a46fc0817
unknown <yaldajafari383@gmail.com>
2025-07-01 00:28:49 +0430

    change some RTLs

--- assembler/assembletest.s
+++ assembler/assembletest.s
@@ -1,6 +1,3 @@
 li x6, 0x0F0F0F0F   # x6 = 0x0F0F0F0F
 li x7, 0x00FF00FF   # x7 = 0x00FF00FF
-xor x8, x6, x7      # x8 = xor of x6 and x7
-or  x9, x6, x7      # x9 = or
 and x10, x6, x7     # x10 = and
-not x11, x10        # x11 = ~x10 (xori with -1)
