6cf1003167f9c54b81138ce065cb1e7c1674f23a
unknown <yaldajafari383@gmail.com>
2025-07-02 12:00:03 +0430

    Troublshooting Branches RTLS

--- assembler/assembletest.s
+++ assembler/assembletest.s
@@ -1,6 +1,6 @@
-    li x1, -1        
-    li x2, 10        
-    bltu x2, x1, lessu_label
-    li x3, 999
-lessu_label:
-    li x3, 555
+li x1, 20 
+li x2, 10      # x2 = 10
+bltu x1, x2, label   # 
+addi x3, x0, 1      # x3 = 1
+label:
+addi x4, x0, 2      # x4 = 2
